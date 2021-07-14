af286ae616bc37d5d995584032be41284c9c8ebd
Imam Bux <imam.bux@idealo.de>
2021-07-14 21:39:26 +0200

    Refactor to common Crud Interface. Closes #11

--- pet-clinic-data/src/main/java/com/imambux/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/imambux/petclinic/service/VetService.java
@@ -3,14 +3,5 @@ package com.imambux.petclinic.service;
 
 import com.imambux.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
-    
+public interface VetService extends CrudService<Vet, Long> {
 }
