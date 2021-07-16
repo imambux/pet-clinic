c3741a95b20c6f63a38d0d2c9f43e5b08850898b
Imam Bux <imam.bux@idealo.de>
2021-07-16 22:32:29 +0200

    List all owners on Owners Index Page, Fix #15

--- pet-clinic-web/src/main/java/com/imambux/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/imambux/petclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.imambux.petclinic.controllers;
 
+import com.imambux.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"index", "index.html"})
-    String list() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "index", "index.html"})
+    String list(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 
